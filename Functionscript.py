2aab524443d702010231b037febdfe26476a401d
cjissosexy <98486468+cjissosexy@users.noreply.github.com>
2022-02-01 08:49:38 -0800

    Add files via upload

---
+++ Functionscript.py
@@ -0,0 +1,18 @@
+#Function
+def NewFunction()
+
+# For loop
+for num in [2,11,3,4,5,23,0,-50]
+    new=num+7
+    print("num,new")
+   
+#Starement
+    print("Hello World")
+
+#variable
+x=99
+
+#Comment
+#Obmaa's last name
+
+    
